c7102e8ab580b04a88f4bcd5109a8a8011588a2e
Ryan Murray <ryanmurray@Ryans-MacBook-Pro.local>
2017-12-11 21:19:35 -0800

    Changed Safari pin color

--- contact/index.php
+++ contact/index.php
@@ -73,7 +73,7 @@ if(intval($responseKeys["success"]) !== 1) {
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
     <link rel="manifest" href="/favicon/manifest.json">
-    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#ef3e33">
     <link rel="shortcut icon" href="/favicon/favicon.ico">
     <meta name="msapplication-config" content="/favicon/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
